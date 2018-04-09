4486478609bd7b621ff8083f5f7ab98a80a8e8dc
phucph <32473982+phucph@users.noreply.github.com>
2018-04-09 20:25:51 +0700

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -1,28 +1,60 @@
 	var rIndex,table=document.getElementById("table");
 	var i;
 function add(){
-	var newRow =table.insertRow(table.length),
-	cell1=newRow.insertCell(0),
-	cell2=newRow.insertCell(1),
-	cell3=newRow.insertCell(2),
-	cell4=newRow.insertCell(3),
-	cell5=newRow.insertCell(4),
+	
 	tensp=document.getElementById("tensp").value,
 	gia=document.getElementById("gia").value,
 	anh=document.getElementById("img").value,
 	thutu=document.getElementById("thutu").value,
 	ghichu=document.getElementById("ghichu").value;
 	if(tensp==""){
-		alert("chua nhap ten ");
+		document.getElementById('span1').innerHTML = " Chưa nhập Họ tên. Vui lòng nhập ! ";
+		return false;
 	}
 	else{
-	cell1.innerHTML=tensp;
+		document.getElementById('span1').innerHTML = "";
 	}
+	if(gia==""){
+	document.getElementById('span2').innerHTML = " Chưa nhập Giá. Vui lòng nhập ! ";
+		return false;
+	}
+	else{
+		document.getElementById('span2').innerHTML = "";
+	}
+	if(anh==""){
+	document.getElementById('span3').innerHTML = " Chưa nhập Tên Anh. Vui lòng nhập ! ";
+		return false;
+	}
+	else{
+		document.getElementById('span3').innerHTML = "";
+	}
+	if(thutu==""){
+	document.getElementById('span4').innerHTML = " Chưa nhập Thứ tự. Vui lòng nhập ! ";
+		return false;
+	}
+	else{
+		document.getElementById('span4').innerHTML = "";
+	}
+	if(ghichu==""){
+	document.getElementById('span5').innerHTML = " Chưa nhập Ghi Chú. Vui lòng nhập ! ";
+		return false;
+	}
+	else{
+	document.getElementById('span5').innerHTML = "";
+	var newRow =table.insertRow(table.length),
+	cell1=newRow.insertCell(0),
+	cell2=newRow.insertCell(1),
+	cell3=newRow.insertCell(2),
+	cell4=newRow.insertCell(3),
+	cell5=newRow.insertCell(4);
+	cell1.innerHTML=tensp;
 	cell2.innerHTML=gia;
 	cell3.innerHTML="<img width='60px' src='images/"+anh+"'>";
 	cell4.innerHTML=thutu;
-	cell5.innerHTML=ghichu;	
+	cell5.innerHTML=ghichu;
 	selected();
+	return kt=true;
+	}
 }
 function selected(){
 	
